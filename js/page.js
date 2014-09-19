12e0094caf80ab51a09ec56c06f026e80eb8587f
Brad Mason <deadlybrad42@gmail.com>
2014-09-18 23:32:40 -0400

    Uses Sammy.js to serve the different 'pages'

--- js/page.js
+++ js/page.js
@@ -1,5 +1,28 @@
 (function($, sammy){
+	// initialize the application
+	var app = $.sammy('#content', function() {
+
+		this.get('/', function() {
+			this.$element().html('A new route! (default)');
+			//redirect('/about');
+		});
+
+		this.get('/about', function() {
+			this.$element().html('A new route! (about)');
+		});
+
+		this.get('/games', function() {
+			this.$element().html('A new route! (games)');
+		});
+
+		this.get('/projects', function() {
+			this.$element().html('A new route! (projects)');
+		});
+	});
+
 	$(function(){
-		console.log("page loaded!");
+		// start the application
+		app.run('/');
+		console.log('starting sammy...');
 	});
 })(jQuery, Sammy);
