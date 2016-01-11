8c7771c88869ec313001a4ebaf892eef311d4862
Brad Mason <deadlybrad42@gmail.com>
2016-01-10 21:41:49 -0500

    Changes indent from 2 spaces to 4

--- scripts/page.js
+++ scripts/page.js
@@ -1,44 +1,44 @@
 (function($, sammy, analytics){
-	// initialize the application
-	var app = $.sammy('#content > div', function() {
+    // initialize the application
+    var app = $.sammy('#content > div', function() {
 
-		this.get('/', function() {
-			loadContent('#about', this.$element());
-			analytics('/');
-		});
+        this.get('/', function() {
+            loadContent('#about', this.$element());
+            analytics('/');
+        });
 
-		this.get('/#about', function() {
-			loadContent('#about', this.$element());
-			analytics('/#about');
-		});
+        this.get('/#about', function() {
+            loadContent('#about', this.$element());
+            analytics('/#about');
+        });
 
-		this.get('/#games', function() {
-			loadContent('#games', this.$element());
-			analytics('/#games');
-		});
+        this.get('/#games', function() {
+            loadContent('#games', this.$element());
+            analytics('/#games');
+        });
 
-		this.get('/#projects', function() {
-			loadContent('#projects', this.$element());
-			analytics('/#projects');
-		});
+        this.get('/#projects', function() {
+            loadContent('#projects', this.$element());
+            analytics('/#projects');
+        });
 
-		var loadContent = function(source, $destination) {
-			var speed = 200,
-				animateOptions = { height: 'toggle', opacity: 'toggle' },
-				animateEasing = 'swing';
+        var loadContent = function(source, $destination) {
+            var speed = 200,
+                animateOptions = { height: 'toggle', opacity: 'toggle' },
+                animateEasing = 'swing';
 
-			$destination.animate(animateOptions, speed, animateEasing, function(){
-				$destination.html($(source).html());
-				$destination.animate(animateOptions, speed, animateEasing);
-			});
+            $destination.animate(animateOptions, speed, animateEasing, function(){
+                $destination.html($(source).html());
+                $destination.animate(animateOptions, speed, animateEasing);
+            });
 
-		};
-	});
+        };
+    });
 
-	$(function(){
-		// start the application
-		app.run('/');
+    $(function(){
+        // start the application
+        app.run('/');
 
-		console.log("Hello there! If you're here to read the source, it might be easier to do that over at https://github.com/DeadlyBrad42/bradmason.net :)");
-	});
+        console.log("Hello there! If you're here to read the source, it might be easier to do that over at https://github.com/DeadlyBrad42/bradmason.net :)");
+    });
 })(jQuery, Sammy, gaLog);
